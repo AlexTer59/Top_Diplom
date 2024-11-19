ab5be1a98a2ae19ec9488f206635bdbd0bc765a6
AlexTer59 <alex.ter.61660z2@gmail.com>
2024-11-19 13:39:34 +0500

    Created models

---
+++ core/models.py
@@ -0,0 +1,85 @@
+from django.db import models
+from user.models import Profile
+
+
+class Board(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название доски")
+    description = models.TextField(blank=True,
+                                   null=True,
+                                   verbose_name="Описание")
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True,
+                                      verbose_name="Дата обновления")
+    owner = models.ForeignKey(Profile,
+                              on_delete=models.CASCADE,
+                              related_name="owned_boards",
+                              verbose_name="Владелец")
+    members = models.ManyToManyField(Profile,
+                                     related_name="shared_boards",
+                                     verbose_name="Участники",
+                                     blank=True)
+
+    class Meta:
+        verbose_name = 'Доска'
+        verbose_name_plural = 'Доски'
+
+    def __str__(self):
+        return self.name
+
+class List(models.Model):
+    name = models.CharField(max_length=255,
+                            verbose_name="Название списка")
+    board = models.ForeignKey(Board,
+                              on_delete=models.CASCADE,
+                              related_name='lists',
+                              verbose_name="Доска")
+    position = models.PositiveIntegerField(default=0,
+                                           verbose_name="Позиция")
+
+    class Meta:
+        verbose_name = 'Список'
+        verbose_name_plural = 'Списки'
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    title = models.CharField(max_length=255,
+                             verbose_name="Название задачи")
+    description = models.TextField(blank=True,
+                                   null=True,
+                                   verbose_name="Описание")
+    list = models.ForeignKey(List,
+                             on_delete=models.CASCADE,
+                             related_name='tasks',
+                             verbose_name="Список")
+    position = models.PositiveIntegerField(default=0, verbose_name="Позиция")
+    due_date = models.DateTimeField(blank=True,
+                                    null=True,
+                                    verbose_name="Срок выполнения")
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True,
+                                      verbose_name="Дата обновления")
+    labels = models.JSONField(default=dict,
+                              blank=True,
+                              verbose_name="Метки")
+    assigned_to = models.ForeignKey(Profile,
+                                    on_delete=models.SET_NULL,
+                                    null=True,
+                                    blank=True,
+                                    related_name='tasks',
+                                    verbose_name='Исполнитель')
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
---
+++ user/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                related_name='profile')
+    avatar = models.ImageField(upload_to='avatars/',
+                               blank=True,
+                               null=True,
+                               verbose_name="Аватар")
+    bio = models.TextField(blank=True,
+                           null=True,
+                           verbose_name='О себе')
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
+    def __str__(self):
+        return self.user.username
